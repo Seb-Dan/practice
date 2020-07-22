0830b9133e77b5fc85a33628bf796ba2aa99dd79
Sebastian <skybastianx@gmail.com>
2020-07-22 22:33:52 +0200

    Exercise 6

--- Chapter 3.py
+++ Chapter 3.py
@@ -9,3 +9,36 @@ for f in xs:
 print(add)
 #Exercise 6
 #Use for loops to make a turtle draw these regular polygons (regular means all sides the same lengths, all angles the same):
+#An equilateral triangle
+#A square
+#A hexagon (six sides)
+#An octagon (eight sides)
+import turtle
+wn = turtle.Screen()
+tess=turtle.Turtle()
+tess.shape("turtle")
+tess.color("gray")
+
+#Triunghi Echilateral
+# for i in range(3):
+#     tess.forward(200)
+#     tess.left(120)
+# wn.mainloop()
+
+#Patrat
+# for i in range(4):
+#     tess.forward(200)
+#     tess.left(90)
+# wn.mainloop()
+
+#Hexagon
+# for i in range(6):
+#     tess.forward(200)
+#     tess.left(60)
+# wn.mainloop()
+
+#Octogon
+# for i in range(8):
+#     tess.forward(100)
+#     tess.left(45)
+# wn.mainloop()
