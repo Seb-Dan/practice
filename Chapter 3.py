6160427171e95d25917b3f63cb184c09c14dfed4
Sebastian <skybastianx@gmail.com>
2020-07-22 22:15:20 +0200

    Exercise 6

--- Chapter 3.py
+++ Chapter 3.py
@@ -1,7 +1,11 @@
-#Exercise 5
+#Exercise 5.d
+#Assume you have the assignment xs = [12, 10, 32, 3, 66, 17, 42, 99, 20]
+#Print the product of all the numbers in the list. (product means all multiplied together)
 xs = [12, 10, 32, 3, 66, 17, 42, 99, 20]
 add=1
 
 for f in xs:
     add=add*f
 print(add)
+#Exercise 6
+#Use for loops to make a turtle draw these regular polygons (regular means all sides the same lengths, all angles the same):
